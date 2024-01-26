a1ce51a84af3c76b009e5f1a882832ac71874bdc
beshoy66q <98409221+beshoy66q@users.noreply.github.com>
2024-01-26 14:39:40 +0200

    add  header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./header.styles.css"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import logo from "../../imgs/Logo.png";
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+const HeaderComponent = () => {
+    return (
+        <>
+            <div className="alert">
+                <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <a href="#">Shop Now</a></p>
+                <span>English <ExpandMoreIcon /></span>
+            </div>
+            <header>
+                <div className="header-left">
+                    <a href="#home" className="logo"><img src={logo} alt="" /></a>
+                </div>
+                <div className="header-middle">
+                    <ul className="nav">
+                        <li><a href="#home" className='active'>Home</a></li>
+                        <li><a href="#home">Contact</a></li>
+                        <li><a href="#home">About</a></li>
+                        <li><a href="#home">Sign Up</a></li>
+                    </ul>
+                </div>
+                <div className="header-right">
+                    <div className="search-box">
+                        <input type="text" placeholder='What are you looking for?'/>
+                      <SearchIcon className='icon'/>
+                    </div>
+                    <FavoriteBorderIcon className='heart-icon'/>
+                    <ShoppingCartIcon className='cart-icon'/>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default HeaderComponent;
